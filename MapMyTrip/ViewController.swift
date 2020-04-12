a9abfd457ac118b82c5bb60bc5c2c40af3c2350f
loganffrost <loganffrost@gmail.com>
2020-04-12 15:21:24 +0100

    Initial Commit

---
+++ MapMyTrip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapMyTrip
+//
+//  Created by Alex on 12/04/2020.
+//  Copyright © 2020 Alex Sykes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
