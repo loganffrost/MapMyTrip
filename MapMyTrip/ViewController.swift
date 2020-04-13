74c44524dc7216dbbf2290ff66d136bbd569eb4c
loganffrost <loganffrost@gmail.com>
2020-04-13 08:34:53 +0100

    Initial commit

--- MapMyTrip/ViewController.swift
+++ MapMyTrip/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    // MARK: Properties
+    var locationManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        locationManager.delegate = self;
+        
+        // user activated automatic authorization info mode
+        var status = CLLocationManager.authorizationStatus()
+        if status == .notDetermined || status == .denied || status == .authorizedWhenInUse {
+               // present an alert indicating location authorization required
+               // and offer to take the user to Settings for the app via
+               // UIApplication -openUrl: and UIApplicationOpenSettingsURLString
+               locationManager.requestAlwaysAuthorization()
+               locationManager.requestWhenInUseAuthorization()
+           }
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
     }
 
 
