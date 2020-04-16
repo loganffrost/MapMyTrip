4eebeb26b63c87cfc737c499786a41056ecd1f59
loganffrost <loganffrost@gmail.com>
2020-04-16 20:47:20 +0100

    Track CLLocation array added

--- MapMyTrip/TrackDetailViewController.swift
+++ MapMyTrip/TrackDetailViewController.swift
@@ -7,18 +7,52 @@
 //
 
 import UIKit
+import CoreLocation
+import Foundation
 
 class TrackDetailViewController: UIViewController {
     // MARK: Properties
     var file: URL?
     var fileData: [String]!
-
+    var track: [CLLocation]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
         fileData = readFile(url: file!)
+        track = convertToCLLocation(fileData: fileData)
+    }
+    
+    func  convertToCLLocation(fileData :[String]) -> [CLLocation] {
+        var theLocations: [CLLocation] = []
+        var theLocation: CLLocation!
+        var coordinate: CLLocationCoordinate2D
+        var elevation: Double!
+        var latitude: Double!
+        var longitude: Double!
+        var hacc: Double!
+        var vacc: Double!
+        var timestampS: String!
         
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        var values :[String] = []
+        for line in fileData {
+            values  = line.components(separatedBy: ",")
+            latitude = Double(values[0])
+            longitude = Double(values[1])
+            hacc = Double(values[2])
+            vacc = Double(values[3])
+            elevation = Double(values[4])
+            timestampS = values[5]
+
+            let date = formatter.date(from: timestampS)
+            coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            theLocation = CLLocation(coordinate: coordinate, altitude: elevation, horizontalAccuracy: hacc, verticalAccuracy: vacc, timestamp: date ?? Date())
+            theLocations.append(theLocation)
+        }
+        return theLocations
     }
     
     // Read trackdata from storage
@@ -36,26 +70,26 @@ class TrackDetailViewController: UIViewController {
         }
         return points
     }
-
+    
     /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+     
+     // Configure the cell...
+     
+     return cell
+     }
+     */
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
